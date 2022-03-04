0a080de0f993f691da5e6e6fe53d2bc64f42554c
guhkun13 <guhkun13@gmail.com>
2022-03-04 21:11:41 +0800

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,7 @@
-var chalk = require('chalk')
+// var chalk = require('chalk')
 
-console.log(
-  chalk.green('[nestjs-typeorm-paginate] =>'),
-  'Please vote for your prefered method of default querying!',
-  chalk.blue('https://github.com/nestjsx/nestjs-typeorm-paginate/issues/576'),
-)
+// console.log(
+//   chalk.green('[nestjs-typeorm-paginate] =>'),
+//   'Please vote for your prefered method of default querying!',
+//   chalk.blue('https://github.com/nestjsx/nestjs-typeorm-paginate/issues/576'),
+// )
